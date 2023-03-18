e85021b84974ae3925524ea1a92d9d96290ce582
Muhammad Awais <muhammad.awais@authoritypartners.com>
2023-03-18 23:55:10 +0500

    Enclosed API call in Try Catch

--- chatclient.py
+++ chatclient.py
@@ -4,22 +4,26 @@ openai.api_key = "PasteYourAPIKeyHere"
 
 chat_prompt = ""
 
-while(True):
+try:
+    while True:
 
-    chat_prompt = input("Enter message for Chat GPT: ")
+        chat_prompt = input("Enter message for Chat GPT: ")
 
-    if chat_prompt.lower() == "#exit":
-        break
+        if chat_prompt.lower() == "#exit":
+            break
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                {"role": "user", "content": chat_prompt},
-            ]
-    )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                    {"role": "user", "content": chat_prompt},
+                ]
+        )
 
-    result = ''
-    for choice in response.choices:
-        result += choice.message.content
+        result = ''
+        for choice in response.choices:
+            result += choice.message.content
 
-    print(result)
+        print(result)
+
+except Exception:
+    print(str(Exception))
